a90089ee975f2d7a38ec1fd084e2d80e1fb66966
anup <anuppal602@gmail.com>
2022-12-23 00:23:02 +0530

    strated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect} from 'react'
 
-function App() {
+import { AppBar, Typography, Grow, Grid, Container } from '@mui/material'
+import memories from './images/memories.jpg'
+import Posts from './components/Posts/Posts'
+import Form from './components/Form/Form'
+import { useDispatch } from 'react-redux'
+import { getPosts } from './actions/posts'
+import useStyles from './styles'
+export const App = () => {
+  const classes = useStyles()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(getPosts())
+  }  , [dispatch])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img src={memories} className={classes.image} alt="memories"  height="60" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
+  )
 }
-
-export default App;
+export default App
---
+++ src/components/Posts/Post/Post.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Post = () => {
+  return (
+    <h1>POSTS</h1>
+  )
+}
+
+export default Post
